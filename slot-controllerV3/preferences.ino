a0677cb6580dcafad89a859214b831116952ac09
slngshotx <slngshotx@gmail.com>
2023-04-16 14:42:42 +0100

    Basic Implementation

---
+++ slot-controllerV3/preferences.ino
@@ -0,0 +1,94 @@
+/*
+ * Arduino slot car controller V3
+ *
+ * Model ESP32 - Preferences module 
+ * Read and write config preferences to flash memory using 
+ *
+ * Copyright 2023 Slingshot Software All rights reserved.
+ *
+ * PWM controller 3 software for use on a ESP32 chip
+ *
+ */
+#include <Preferences.h>
+
+/* 
+ * getPreferences
+ *
+ * Read any stored preferences from flash. Defaults will be used if not set.
+ */
+void getPreferences() {
+  Preferences controllerPrefs;
+
+  controllerPrefs.begin("controlPrefs", true);
+
+  iPWMFrequency = controllerPrefs.getInt("sPWMFreq", __DEF_PWM_FREQUENCY__);
+  iBrakeSetting = controllerPrefs.getInt("sBrake", __DEF_BRAKE_SETTING__);
+  iThrottleSetting = controllerPrefs.getInt("sThrottle", __DEF_THROTTLE_SETTING__);
+  iTCSSetting = controllerPrefs.getInt("sTCS", __DEF_TCS_SETTING__);
+  iTCSStartSetting = controllerPrefs.getInt("sTCSStart", __DEF_TCS_START_SETTING__);
+  iTCSStopSetting = controllerPrefs.getInt("sTCSStop", __DEF_TCS_STOP_SETTING__);
+  iCoastSetting = controllerPrefs.getInt("sCoast", __DEF_COAST_SETTING__);
+  fCurveVal = controllerPrefs.getFloat("sCurveVal", __DEF_CURVEVAL__);
+
+  controllerPrefs.end();
+
+#if defined(__DEBUG__)
+  Serial.println("Read preferences");
+  printPrefValues();
+#endif
+}
+
+/* 
+ * putPreferences
+ *
+ * Save the preferences to persistent store for next time.
+ */
+void putPreferences() {
+  Preferences controllerPrefs;
+
+  controllerPrefs.begin("controlPrefs", false);
+
+  controllerPrefs.putInt("sPWMFreq", iPWMFrequency);
+  controllerPrefs.putInt("sBrake", iBrakeSetting);
+  controllerPrefs.putInt("sThrottle", iThrottleSetting);
+  controllerPrefs.putInt("sTCS", iTCSSetting);
+  controllerPrefs.putInt("sTCSStart", iTCSStartSetting);
+  controllerPrefs.putInt("sTCSStop", iTCSStopSetting);
+  controllerPrefs.putInt("sCoast", iCoastSetting);
+  controllerPrefs.putFloat("sCurveVal", fCurveVal);
+
+  controllerPrefs.end();
+
+#if defined(__DEBUG__)
+  Serial.println("Saved preferences");
+  printPrefValues();
+#endif
+
+  getPreferences();
+  // Reset the PWM values if they have changed
+  setupPWM();
+}
+
+/* 
+ * Only used when __DEBUG__ is enabled to display
+ */
+#if defined(__DEBUG__)
+void printPrefValues() {
+  // print the results to the serial monitor:
+  Serial.print("PWMFrequency: ");
+  Serial.println(iPWMFrequency);
+  Serial.print("BrakeSetting: ");
+  Serial.println(iBrakeSetting);
+  Serial.print("ThrottleSetting: ");
+  Serial.println(iThrottleSetting);
+  Serial.print("TCSSetting: ");
+  Serial.println(iTCSSetting);
+  Serial.print("TCSStartSetting: ");
+  Serial.println(iTCSStartSetting);
+  Serial.print("TCSStopSetting: ");
+  Serial.println(iTCSStopSetting);
+  Serial.print("CurveVal: ");
+  Serial.println(fCurveVal);
+}
+#endif
+
---
+++ slot-controllerV3/webserver.ino
@@ -0,0 +1,195 @@
+/*
+ * Arduino slot car controller V3
+ *
+ * Model ESP32 - Webserver module 
+ * Configure and run a local webserver to change config values. 
+ *
+ * Copyright 2023 Slingshot Software All rights reserved.
+ *
+ * PWM controller 3 software for use on a ESP32 chip
+ *
+ */
+#include <WiFi.h>
+
+/* Put your SSID & Password */
+const char* ssid = "slingshot-controller";
+const char* password = "slingshot";
+
+/* Put IP Address details */
+IPAddress local_ip(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+/* 
+ * startWebserver
+ *
+ * If required startup the webserver as an access point.
+ * Connect a device to ssid/password and browse to local_ip for access to controller settings
+ */
+void startWebserver() {
+#if defined(__DEBUG__)
+  Serial.print("Starting webserver: ");
+  Serial.println(ssid);
+#endif
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+#if defined(__DEBUG__)
+  Serial.print("Webserver started: ");
+  Serial.println(local_ip);
+#endif
+
+  server.on("/", handleOnConnect);
+  server.on("/put", handleSubmit);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+}
+
+/* 
+ * handleSubmit
+ *
+ * Process webserver data from a submit
+ */
+void handleSubmit() {
+  String sPWMFrequency = server.arg("sPWMFreq");
+  String sBrakeSetting = server.arg("sBrake");
+  String sThrottleSetting = server.arg("sThrottle");
+  String sTCSSetting = server.arg("sTCS");
+  String sTCSStartSetting = server.arg("sTCSStart");
+  String sTCSStopSetting = server.arg("sTCSStop");
+  String sCoastSetting = server.arg("sCoast");
+  String sCurveVal = server.arg("sCurveVal");
+
+  if (sPWMFrequency)
+    iPWMFrequency = sPWMFrequency.toInt();
+  if (sBrakeSetting)
+    iBrakeSetting = sBrakeSetting.toInt();
+  if (sThrottleSetting)
+    iThrottleSetting = sThrottleSetting.toInt();
+  if (sTCSSetting)
+    iTCSSetting = sTCSSetting.toInt();
+  if (sTCSStartSetting)
+    iTCSStartSetting = sTCSStartSetting.toInt();
+  if (sTCSStopSetting)
+    iTCSStopSetting = sTCSStopSetting.toInt();
+  if (sTCSStopSetting)
+    iCoastSetting = sCoastSetting.toInt();
+  if (sCurveVal)
+    fCurveVal = sCurveVal.toFloat();
+
+#if defined(__DEBUG__)
+  Serial.print("Number of params ");
+  Serial.println(server.args());
+  Serial.print("PWMFrequency : ");
+  Serial.println(iPWMFrequency);
+  Serial.print("BrakeSetting : ");
+  Serial.println(iBrakeSetting);
+  Serial.print("ThrottleSetting : ");
+  Serial.println(iThrottleSetting);
+  Serial.print("TCSSetting : ");
+  Serial.println(iTCSSetting);
+  Serial.print("TCSStartSetting : ");
+  Serial.println(iTCSStartSetting);
+  Serial.print("TCSStopSetting : ");
+  Serial.println(iTCSStopSetting);
+  Serial.print("Curve Value : ");
+  Serial.println(fCurveVal);
+#endif
+
+  // Update the preferences
+  putPreferences();
+  server.send(200, "text/html", SendHTML());
+}
+
+/* 
+ * handleOnConnect
+ *
+ * Display the default web page for controller config.
+ */
+void handleOnConnect() {
+#if defined(__DEBUG__)
+  Serial.println("Sending HTML");
+#endif
+  server.send(200, "text/html", SendHTML());
+}
+
+void handleNotFound() {
+  server.send(404, "text/plain", "Controller page Not found");
+}
+
+String SendHTML() {
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>Slingshot Controller Config</title>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<h2>Slingshot Controller V3 Config</h2>\n";
+  ptr += "<form action=\"/put\">";
+ 
+  ptr += "<label> PWM Freq(Hz) (200-25000) </label>";
+  ptr += "<input type=\"range\" min=\"200\" max=\"25000\" step=\"100\" name=\"sPWMFreq\" oninput=\"this.nextElementSibling.value = this.value\" value=\"";
+  ptr += String(iPWMFrequency);
+  ptr += "\"> <output>";
+  ptr += String(iPWMFrequency);
+  ptr += "</output><br>";
+
+  ptr += "<label> Brakes (0-255) </label>";
+  ptr += "<input type=\"range\" min=\"0\" max=\"255\" name=\"sBrake\" oninput=\"this.nextElementSibling.value = this.value\" value=\"";
+  ptr += String(iBrakeSetting);
+  ptr += "\"> <output>";
+  ptr += String(iBrakeSetting);
+  ptr += "</output><br>";
+
+  ptr += "==== BELOW NOT CURRENTLY IMPLEMENTED ===";
+
+  ptr += "<label> Throttle(%) (0-100) </label>";
+  ptr += "<input type=\"range\" min=\"0\" max=\"100\" name=\"sThrottle\" oninput=\"this.nextElementSibling.value = this.value\" value=\"";
+  ptr += String(iThrottleSetting);
+  ptr += "\"> <output>";
+  ptr += String(iThrottleSetting);
+  ptr += "</output><br><hr>";
+
+  ptr += "<label> Min Speed (0-255) </label>";
+  ptr += "<input type=\"range\" min=\"0\" max=\"255\" name=\"sCoast\" oninput=\"this.nextElementSibling.value = this.value\" value=\"";
+  ptr += String(iCoastSetting);
+  ptr += "\"> <output>";
+  ptr += String(iCoastSetting);
+  ptr += "</output><br><hr>";
+
+  ptr += "<label> Curve exp (0.5->1<-2) 1 is flat</label>";
+  ptr += "<input type=\"text\" min=\"0.5\" max=\"2\" id=\"sCurveVal\" name=\"sCurveVal\" value=\"";
+  ptr += String(fCurveVal);
+  ptr += "\"><br>";
+
+  ptr += "<label> TCS (0-100) </label>";
+  ptr += "<input type=\"range\" min=\"0\" max=\"100\" name=\"sTCS\" oninput=\"this.nextElementSibling.value = this.value\" value=\"";
+  ptr += String(iTCSSetting);
+  ptr += "\"> <output>";
+  ptr += String(iTCSSetting);
+  ptr += "</output><br>";
+
+  ptr += "<label> Start TCS (0-255) </label>";
+  ptr += "<input type=\"range\" min=\"0\" max=\"255\" name=\"sTCSStart\" oninput=\"this.nextElementSibling.value = this.value\" value=\"";
+  ptr += String(iTCSStartSetting);
+  ptr += "\"> <output>";
+  ptr += String(iTCSStartSetting);
+  ptr += "</output><br>";
+
+  ptr += "<label> End TCS (0-255) </label>";
+  ptr += "<input type=\"range\" min=\"0\" max=\"255\" name=\"sTCSStop\" oninput=\"this.nextElementSibling.value = this.value\" value=\"";
+  ptr += String(iTCSStopSetting);
+  ptr += "\"> <output>";
+  ptr += String(iTCSStopSetting);
+  ptr += "</output><br>";
+
+  ptr += "<br><input type=\"submit\" value=\"Submit\"></form>";
+  ptr += "</body>\n";
+  ptr += "</html>\n";
+
+  return ptr;
+}
